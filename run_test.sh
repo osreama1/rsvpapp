81f3f02f632352d9143820886a32da8740c4ecbb
osreama1 <38256631+osreama1@users.noreply.github.com>
2018-04-22 12:34:18 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 virtualenv rsvpapp --system-site-packages -v
 source rsvpapp/bin/activate
-pip install flask
-pip install pymongo
-pip install pytest
-pip install mongomock
+python -m ensurepip --default-pip
+python -m pip install --upgrade pip setuptools wheel
+pip --version
+pip install -r requirements.txt
+pytest tests/test_rsvpapp.py
+
 
