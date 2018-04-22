86df17b5bc2b8c9c7df185e7998f47bef4167a43
osreama1 <38256631+osreama1@users.noreply.github.com>
2018-04-22 12:40:17 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 virtualenv rsvpapp --system-site-packages -v
 source rsvpapp/bin/activate
-python -m ensurepip --default-pip
-python -m pip install --upgrade pip setuptools wheel
-pip --version
-pip install -r requirements.txt
-pytest tests/test_rsvpapp.py
+python -m pip 
 
 
